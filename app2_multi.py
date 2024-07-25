665ab0cdea421fd639f2d439749789217a772731
NourSouki <122490141+NourSouki@users.noreply.github.com>
2024-07-25 18:03:08 +0100

    Update app2_multi.py

--- app2_multi.py
+++ app2_multi.py
@@ -38,7 +38,6 @@ name, authentication_status, username = authenticator.login()
 if authentication_status:
     authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{name}*')
-    st.title('Some content')
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
